a2a7dbb83c521548fdf3e7aad651119c4dae0688
Juanma Ulman <juanmaulman@gmail.com>
2022-11-14 02:32:26 -0300

    Mejoras en las img de las card

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
+// import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import "./Item.css";
@@ -18,15 +18,16 @@ const MultiActionAreaCard = ({ img, title, description, price, id }) => {
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "space-between",
-
+          
         }}
       >
-        <CardMedia
+        <img src={img}  className="img img-fluid" alt="" />
+        {/* <CardMedia
           component="img"
           image={img}
           alt="img"
-          className="img"
-        />
+          className="img img-fluid"
+        /> */}
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {title}
